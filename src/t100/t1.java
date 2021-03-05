c6f208fa52b7ed0f99d97c4a6924ae15ca9b5423
EDITH768 <sssrajeshs23@gmail.com>
2021-03-05 17:23:53 +0530

    test

--- src/t100/t1.java
+++ src/t100/t1.java
@@ -1,17 +1,30 @@
 package t100;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 public class t1 {
 
-  @Parameters
+
+public static WebDriver driver;
+  @Parameters({"Browser","url"})
     @BeforeClass
-void start(){
+void start(String Browser){
+      if (Browser.equalsIgnoreCase("Chrome"))
+      {
     System.getProperty("webdriver.chrome.driver","C:\\Users\\CATASTROS\\IdeaProjects\\test\\src\\t100\\chromedriver.exe");
+    driver=new ChromeDriver();}else
+        if (Browser.equalsIgnoreCase("Firefox")){
+            System.getProperty("webdriver.gecko.driver","C:\\Users\\CATASTROS\\IdeaProjects\\test\\src\\t100\\chromedriver.exe");
+            driver=new ChromeDriver();
+        }
+
+
 }
     @BeforeMethod
-    void JDBC(){
-
+    void URL(String url){
+driver.get(url);
     }
 
   @Test
@@ -20,11 +33,11 @@ void start(){
         }
     @AfterMethod
    void JDBCQuit(){
-
+System.out.println("After Method");
     }
 
     @AfterClass
     void quit(){
-
+driver.quit();
     }
 }
